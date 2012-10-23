e36edd1210ce0783b6d826db209c6ebfdf755439
Jeffrey Clark <jeff@the1010collective.com>
2012-10-22 22:10:49 -0700

    Adding New files

--- wp-content/themes/bckspace/index.php
+++ wp-content/themes/bckspace/index.php
@@ -29,7 +29,7 @@
 			<h2>Page Not Found</h2>			
 			<p>Looks like the page you're looking for isn't here anymore. Try browsing the <a href="">categories</a>, <a href="">archives</a>, or using the search box below.</p>
 			
-			<?php include(TEMPLATEPATH.'/searhform.php'); ?>		
+			<?php include(TEMPLATEPATH.'/searchform.php'); ?>		
 		</div> <!-- .post -->
 
 		<?php endif; ?>
--- wp-content/themes/bckspace/template-home.php
+++ wp-content/themes/bckspace/template-home.php
@@ -11,12 +11,45 @@
 
 get_header(); ?>
     <div class="wrapper">
+        <hr>
         <div class="main-content">
             <div class="nivo">
                 <?php if( function_exists('bck_slider_build') ) : bck_slider_build(); endif; ?>
             </div>
+            
+            <hr>
+            
             <div class="sub-section">
-                
+                <?php 
+                global $post;
+                $args = array(
+                    'showposts' => 3, 
+                    'post_type' => 'page',
+                    'post__in' => array(18, 20, 90)
+                );
+                $query = new WP_Query($args); 
+                while($query->have_posts() ) : $query->the_post();
+                    echo '<div class="cta-home">';
+                    echo '<h2 entry-title>';
+                    the_title();
+                    echo '</h2>';
+                    echo '<a href="';
+                    the_permalink();
+                    echo '">';
+                    the_post_thumbnail('cta-home');
+                    echo "</a>";
+                    the_excerpt();
+                    
+                    if($post->ID == 90 ) {
+                        echo do_shortcode('[contact-form-7 id="785" title="Email Signup"]');
+                        echo '<div class="social">';
+                        echo '<a href="' . get_option('facebook_account') . '"><img src="'.get_template_directory_uri().'/images/VegasCoffeeBean.jpeg" border="0"/></a>';
+                        echo '<a href="' . get_option('twitter_account') . '"><img src="'.get_template_directory_uri().'/images/vegascoffeebean-1.jpeg" border="0"/></a>';
+                        echo '</div>';
+                    }
+                    echo '</div>';
+                endwhile;
+                ?>
             </div>
         </div> <!-- end wrapper -->
 <!-- END MAIN CONTENT -->
